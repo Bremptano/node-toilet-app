79619b82410a98fff28a4860ff7b956261dc5643
Bremptano <jensvdn1996@gmail.com>
2019-12-09 19:17:10 +0100

    port change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
