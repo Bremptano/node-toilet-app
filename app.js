1cd567baba4483c2b29728bef0250a3bf3de00fa
Bremptano <jensvdn1996@gmail.com>
2019-12-09 21:12:36 +0100

    app.js

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const app = express();
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
-
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'));
-
-app.use(express.static('public'));
-
-app.get('/', function(req, res){
-  res.render('index');
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
