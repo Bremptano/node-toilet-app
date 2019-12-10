b06591f8d9253bd3bf45439f082216b3f77b2614
Bremptano <jensvdn1996@gmail.com>
2019-12-10 15:10:29 +0100

    start script

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-server.listen(port, () => {
-    console.log("App is running on port " + port);
-});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
